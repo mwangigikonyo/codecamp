66d5ce68316a577fcd5a668d38cd2e1e3c956ae3
Mwangi Gikonyo <mwangi.gikonyo.t@gmail.com>
2024-12-15 15:39:14 +0000

    Update

--- config/env/production.js
+++ config/env/production.js
@@ -265,10 +265,10 @@ module.exports = {
     * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
     *                                                                          *
     ***************************************************************************/
-    // onlyAllowOrigins: [
-    //   'https://example.com',
-    //   'https://staging.example.com',
-    // ],
+    onlyAllowOrigins: [
+       'https://codecamp.sozuri.net',
+       'http://localhost:9444','http://localhost:1444','https://iids.sozuri.net'
+    ],
 
 
     /***************************************************************************

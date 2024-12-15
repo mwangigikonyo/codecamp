066bb3de44af5d9d2242b00b2b15dd09f6edb57e
Ubuntu <ubuntu@ip-10-0-0-48.us-east-2.compute.internal>
2024-12-15 15:27:21 +0000

    Update

--- config/env/production.js
+++ config/env/production.js
@@ -356,7 +356,8 @@ module.exports = {
   *                                                                         *
   ***************************************************************************/
   // port: 80,
-
+  port: process.env.SERVER_PORT || 1444,
+  explicitHost: process.env.SERVER_HOST || 'localhost', 
 
 
   /**************************************************************************
@@ -389,8 +390,8 @@ module.exports = {
   *                                                                         *
   ***************************************************************************/
   custom: {
-    baseUrl: 'https://example.com',
-    internalEmailAddress: 'support@example.com',
+    baseUrl: 'https://codecamp.sozuri.net',
+    internalEmailAddress: 'codecamp.support@sozuri.net',
 
     // sendgridSecret: 'SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU',
     // stripeSecret: 'sk_prod__fake_Nfgh82401348jaDa3lkZ0d9Hm',

7a3e64c6eb1a177421887f21e26c39ae058aa9b9
Mwangi Gikonyo <mwangi.gikonyo.t@gmail.com>
2025-02-06 20:50:05 +0300

    Updated

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Code Camp',
   tagline: 'Tech Stack Trainning',
   url: 'http://localhost:1444',
-  baseUrl: '/doc4/',
+  baseUrl: '/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
